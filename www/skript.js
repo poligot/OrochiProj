36b7544a22387876b4373a5749c731f743923136
poligot <poligot89@gmail.com>
2013-03-30 00:25:37 +0400

    Проверка мыла добавлена

--- www/skript.js
+++ www/skript.js
@@ -20,3 +20,14 @@ $('#test_form').html(result);
 });
 });
 
+function mail_valid() {
+    var mail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    var form_mail = document.getElementById('mail');
+    if (form_mail.value.search(mail)!==-1){
+       alert('ХОРОШ!!!'); 
+    }
+    else{
+        alert("Сударь ваше мыло говно!");
+    }
+        
+}
